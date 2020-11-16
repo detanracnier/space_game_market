5f2099ac0afc08b849e0cb8502bf9caabf4fb91e
Spencer Rosio <spencerrosio3d@gmail.com>
2020-11-15 19:24:50 -0600

    trying to deploy build, redirect

---
+++ build/precache-manifest.b04d25815373770eaeb2945b71b5d166.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a9e9948d4731d508b2e7e20bd3586dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0b824aabea9b5557683",
+    "url": "/static/css/main.983884e1.chunk.css"
+  },
+  {
+    "revision": "2e83bcf7b7e875e68345",
+    "url": "/static/js/2.f98c0228.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f98c0228.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0b824aabea9b5557683",
+    "url": "/static/js/main.f36ec984.chunk.js"
+  },
+  {
+    "revision": "c4010027b85dc2443626",
+    "url": "/static/js/runtime-main.170ad29f.js"
+  },
+  {
+    "revision": "a65f4c30c295715591bf1d5007fd8756",
+    "url": "/static/media/corporation_frigate.a65f4c30.jpg"
+  },
+  {
+    "revision": "277dd71a49a0b4106b22cbdd3f655a2f",
+    "url": "/static/media/federation_frigate.277dd71a.jpg"
+  },
+  {
+    "revision": "9134d25cab5c3868564feaa0c1376c5f",
+    "url": "/static/media/ivorian_frigate.9134d25c.jpg"
+  }
+]);
