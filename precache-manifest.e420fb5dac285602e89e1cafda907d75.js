631326ceedc9725127376a2c30cc55bac60bdc8a
Spencer Rosio <spencerrosio3d@gmail.com>
2020-11-15 19:40:39 -0600

    Updates

---
+++ precache-manifest.e420fb5dac285602e89e1cafda907d75.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0f3bc44e691b1526b5b471ad112fa0a",
+    "url": "/space_game_market/index.html"
+  },
+  {
+    "revision": "69c940b105ae7352b47e",
+    "url": "/space_game_market/static/css/main.983884e1.chunk.css"
+  },
+  {
+    "revision": "2e83bcf7b7e875e68345",
+    "url": "/space_game_market/static/js/2.f98c0228.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/space_game_market/static/js/2.f98c0228.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69c940b105ae7352b47e",
+    "url": "/space_game_market/static/js/main.03fda149.chunk.js"
+  },
+  {
+    "revision": "ac44bb52baed5a57cbf3",
+    "url": "/space_game_market/static/js/runtime-main.ab130184.js"
+  },
+  {
+    "revision": "a65f4c30c295715591bf1d5007fd8756",
+    "url": "/space_game_market/static/media/corporation_frigate.a65f4c30.jpg"
+  },
+  {
+    "revision": "277dd71a49a0b4106b22cbdd3f655a2f",
+    "url": "/space_game_market/static/media/federation_frigate.277dd71a.jpg"
+  },
+  {
+    "revision": "9134d25cab5c3868564feaa0c1376c5f",
+    "url": "/space_game_market/static/media/ivorian_frigate.9134d25c.jpg"
+  }
+]);
